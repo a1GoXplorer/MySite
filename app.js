1988914e731f1451562eb2e295a6ad1f24e90b7d
Mark Daniels <mark_daniels1982@hotmail.com>
2015-07-22 17:34:12 -0700

    add request

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 var express = require('express');
+var mongodb = require('mongodb');
+var request = require('request');
+var path = require('path');
+var db = require('./models');
+
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/data/db", function(err, db) {
+  if(!err) {
+    console.log("**CONNECTED TO MONGO!!**");
+  }
+});
 
 var app = express();
 
@@ -6,6 +20,7 @@ app.get('/', function (req, res) {
   res.send('Fuck You, World.');
 });
 
+//server stuffs
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
